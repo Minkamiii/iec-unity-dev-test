a5fe36c193f8bb1e759ff5069bc55800c1a4160c
unknown <trantheminh3008@gmail.com>
2025-08-27 18:49:07 +0700

    initial commit

---
+++ Assets/Scripts/Utility/Constants.cs
@@ -0,0 +1,24 @@
+﻿public class Constants 
+{
+    public const string GAME_SETTINGS_PATH = "gamesettings";
+
+    public const string PREFAB_CELL_BACKGROUND = "prefabs/cellBackground";
+
+    public const string PREFAB_NORMAL_TYPE_ONE = "prefabs/fishNormal1";
+
+    public const string PREFAB_NORMAL_TYPE_TWO = "prefabs/fishNormal2";
+
+    public const string PREFAB_NORMAL_TYPE_THREE = "prefabs/fishNormal3";
+
+    public const string PREFAB_NORMAL_TYPE_FOUR = "prefabs/fishNormal4";
+
+    public const string PREFAB_NORMAL_TYPE_FIVE = "prefabs/fishNormal5";
+
+    public const string PREFAB_NORMAL_TYPE_SIX = "prefabs/fishNormal6";
+
+    public const string PREFAB_BONUS_HORIZONTAL = "prefabs/itemBonusHorizontal";
+
+    public const string PREFAB_BONUS_VERTICAL = "prefabs/itemBonusVertical";
+
+    public const string PREFAB_BONUS_BOMB = "prefabs/fishBonusBomb";
+}
